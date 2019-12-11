6cca2226f7c9115691ec892a08a262bc0fe3ae08
keyghzz <kmreynoso@up.edu.ph>
2019-12-12 00:44:24 +0800

    some changes

--- main.ino
+++ main.ino
@@ -1,8 +1,8 @@
 //check https://www.teachmemicro.com/use-l298n-motor-driver/ for setup.
-// motor B was used
+// motor A was used
 
-int trigPin = 11;    // Trigger
-int echoPin = 12;    // Echo
+int trigPin = 12;    // Trigger
+int echoPin = 11;    // Echo
 long duration, cm, inches;
 
 void setup() {
@@ -11,23 +11,23 @@ void setup() {
   //Define inputs and outputs
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(7, OUTPUT); //IN3
-  pinMode(6, OUTPUT); //IN4
+  pinMode(7, OUTPUT); //IN1
+  pinMode(6, OUTPUT); //IN2d
 }
 
 void goStraight()   //run both motors in the same direction
 {
-// turn on motor B
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW);
-// set speed to 150 out 255
-// uncomment and remove jumper to customize speed
-// analogWrite(EnB, 200);
-delay(250); //tweak this depending on the rotation of the motor (only 1 step must be done during this time)
-// now turn off motors
-digitalWrite(7, LOW);
-digitalWrite(6, LOW);
-Serial.println("ran");
+  // turn on motor A
+  digitalWrite(7, HIGH);
+  digitalWrite(6, LOW);
+  // set speed to 150 out 255
+  // uncomment and remove jumper to customize speed
+  // analogWrite(EnB, 200);
+  delay(500);//tweak this depending on the rotation of the motor (only 1 step must be done during this time)
+  // now turn off motors
+  digitalWrite(7, LOW);
+  digitalWrite(6, LOW);
+  Serial.println("ran");
 }
 
 void loop() {
@@ -46,8 +46,8 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
 
   // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  cm = (duration / 2) / 29.1;   // Divide by 29.1 or multiply by 0.0343
+  inches = (duration / 2) / 74; // Divide by 74 or multiply by 0.0135
 
   Serial.print(inches);
   Serial.print("in, ");
@@ -55,8 +55,9 @@ void loop() {
   Serial.print("cm");
   Serial.println();
 
-  if(inches > 6.0)
+  if (inches > 10.0)
   {
     goStraight();
   }
+  delay(1000);
 }
